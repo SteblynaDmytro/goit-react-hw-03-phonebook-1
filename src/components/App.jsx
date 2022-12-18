eac3bf181cc452f3264761e903e3b380a27f774d
AnastasiiaSteblyna <98827282+AnastasiiaSteblyna@users.noreply.github.com>
2022-12-18 13:11:52 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import ContactList from './ContactList/ContactList';
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
